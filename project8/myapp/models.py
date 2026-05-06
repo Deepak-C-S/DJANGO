5863611afa8f18e9fa1ef0124dd87c162f7f8ea5
deepak0deepz <deepakcs2k4@gmail.com>
2026-05-06 14:11:56 +0530

    DJANGO MODEL

---
+++ project8/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    age=models.IntegerField()
+    dob=models.DateField()
+    email=models.EmailField()
+    usn=models.CharField(max_length=20)
+    dept=models.CharField(max_length=50)
+    college=models.CharField(max_length=100)
+
+
